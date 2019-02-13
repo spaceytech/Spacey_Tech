fde572af87422b73fa390f815875fb020737a6e4
Zakeria Ali <zak.fined@gmail.com>
2019-02-13 11:58:47 +0000

    Create and Style navigation

--- client/src/Components/Layout/Navigation.js
+++ client/src/Components/Layout/Navigation.js
@@ -2,7 +2,16 @@ import React, { Component } from "react";
 
 class Navigation extends Component {
   render() {
-    return <nav className="navigation" />;
+    return (
+      <nav className="navigation">
+        <h2 className="navigation__logo">spacey</h2>
+        <p className="navigation__items navigation__categories">Categories</p>
+        <p className="navigation__items navigation__works">How it works</p>
+        <p className="navigation__items navigation__sign-up">Sign up</p>
+        <p className="navigation__items navigation__login">Login</p>
+        <p className="navigation__button">Become a tasker</p>
+      </nav>
+    );
   }
 }
 
