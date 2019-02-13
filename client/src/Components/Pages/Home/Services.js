6cf5ea2384e80531d3cea76061893353b98482c2
Zakeria Ali <zak.fined@gmail.com>
2019-02-13 23:17:45 +0000

    Create and style the Services section

--- client/src/Components/Pages/Home/Services.js
+++ client/src/Components/Pages/Home/Services.js
@@ -2,7 +2,30 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Services = props => {
-  return <section className="home__services" />;
+  return (
+    <section className="home__services">
+      <img src="images/back.png" alt="background image" />
+      <h1 className="home__services--title">Our services</h1>
+      <a href="#">
+        <p>Handyman service</p>
+      </a>
+      <a href="#">
+        <p>Laundry service</p>
+      </a>
+      <a href="#">
+        <p>Delivery service</p>
+      </a>
+      <a href="#">
+        <p>End of tenancy service</p>
+      </a>
+      <a href="#">
+        <p>Assembly service</p>
+      </a>
+      <a href="#">
+        <p>Installation service</p>
+      </a>
+    </section>
+  );
 };
 
 export default Services;
