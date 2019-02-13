58bde7f8d9254ad8acca0243bf28fae0579218a9
Zakeria Ali <zak.fined@gmail.com>
2019-02-13 12:31:55 +0000

    Add HTML for header

--- client/src/Components/Pages/Home/Header.js
+++ client/src/Components/Pages/Home/Header.js
@@ -1,8 +1,18 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 const Header = props => {
-  return <header class="home__header" />;
+  return (
+    <header className="home__header">
+      <h1 className="home__header--heading">Get things done and dusted</h1>
+      <p className="home__header--tagline">
+        The most affordable and efficient way to get things done around the home
+      </p>
+      <a href="#" className="home__header--button">
+        <button>Get started</button>
+      </a>
+      <p className="home__header--link">See how it works</p>
+    </header>
+  );
 };
 
 export default Header;
