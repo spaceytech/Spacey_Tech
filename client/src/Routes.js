31de1c982c0405bdf5ffed3f20071bd5c61a06d1
Zakeria Ali <zak.fined@gmail.com>
2019-02-13 10:55:31 +0000

    Build basic layout

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
-import Home from "./Components/Pages/Home";
+import { Switch, Route } from "react-router-dom";
+import Home from "./Components/Pages/Home/Home";
 
 const Routes = () => {
   return (
