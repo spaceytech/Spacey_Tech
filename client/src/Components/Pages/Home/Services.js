f04f868ee508de09deb622cb8cd24349f9d858e6
Zakeria Ali <zak.fined@gmail.com>
2019-02-14 19:11:27 +0000

    Change layout of links

--- client/src/Components/Pages/Home/Services.js
+++ client/src/Components/Pages/Home/Services.js
@@ -4,25 +4,25 @@ import PropTypes from "prop-types";
 const Services = props => {
   return (
     <section className="home__services">
-      <img src="images/back.png" alt="background image" />
+      <img id="top" src="images/back.png" alt="background image" />
       <h1 className="home__services--title">Our services</h1>
-      <a href="#">
-        <p>Handyman service</p>
+      <a href="#" className="button">
+        <button>General Handyman</button>
       </a>
-      <a href="#">
-        <p>Laundry service</p>
+      <a href="#" className="button">
+        <button>Laundry</button>
       </a>
-      <a href="#">
-        <p>Delivery service</p>
+      <a href="#" className="button">
+        <button>Delivery</button>
       </a>
-      <a href="#">
-        <p>End of tenancy service</p>
+      <a href="#" className="button">
+        <button>Cleaning</button>
       </a>
-      <a href="#">
-        <p>Assembly service</p>
+      <a href="#" className="button">
+        <button>Flat pack assembly</button>
       </a>
-      <a href="#">
-        <p>Installation service</p>
+      <a href="#" className="button">
+        <button>Installation</button>
       </a>
     </section>
   );
