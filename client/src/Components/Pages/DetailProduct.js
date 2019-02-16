0cb99983a69ebdb899143087f4ea443846b74a63
Zakeria Ali <zak.fined@gmail.com>
2019-02-16 21:45:10 +0000

    Fix scoll bottom issue

--- client/src/Components/Pages/DetailProduct.js
+++ client/src/Components/Pages/DetailProduct.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import Layout from "../Layout/Layout";
 
 class DetailProduct extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
   render() {
     return (
       <div className="details">
