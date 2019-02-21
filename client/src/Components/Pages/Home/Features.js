79dab61d902d0c6d9465025b8df9a3d0a58191c3
Zakeria Ali <zak.fined@gmail.com>
2019-02-21 16:53:04 +0000

    Fix featured section logos

--- client/src/Components/Pages/Home/Features.js
+++ client/src/Components/Pages/Home/Features.js
@@ -5,7 +5,6 @@ const Features = props => {
   return (
     <section className="home__features">
       <h3 className="home__features--text">As seen on</h3>
-      <img src="images/logo-bbc.png" alt="Wired" className="bbc" />
       <img src="images/logo-bi.png" alt="Business Insider" className="bi" />
       <img src="images/times.png" alt="Tech Crunch" className="times" />
     </section>
