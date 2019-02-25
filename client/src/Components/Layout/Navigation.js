edf351c1cd37758c3b7bda06b96411e989bc0357
Zakeria Ali <zak.fined@gmail.com>
2019-02-25 14:48:00 +0000

    Add a responsive navigation with media queries

--- client/src/Components/Layout/Navigation.js
+++ client/src/Components/Layout/Navigation.js
@@ -30,6 +30,15 @@ class Navigation extends Component {
     }
   };
 
+  openMobileNav = () => {
+    console.log(this.navElement.style.display);
+    if (this.navElement.style.display === "flex") {
+      this.navElement.style.display = "none";
+    } else {
+      this.navElement.style.display = "flex";
+    }
+  };
+
   render() {
     return (
       <nav className="navigation">
@@ -38,8 +47,40 @@ class Navigation extends Component {
             spacey<span style={{ color: "rgb(0, 188, 135)" }}>.</span>
           </h2>
         </Link>
+        <div id="myLinks" ref={element => (this.navElement = element)}>
+          <div
+            className="navigation__items navigation__categories"
+            onMouseEnter={this.mouseEnter}
+            onMouseLeave={this.mouseLeave}
+            style={{
+              borderTop: this.state.showDropdown
+                ? "2px solid var(--light-green)"
+                : ""
+            }}
+          >
+            Categories
+            {this.state.showDropdown ? (
+              <Dropdown
+                style={{ display: "block" }}
+                passRef={this.defineRef}
+                leaveDropDown={this.leaveDropDown}
+                show={this.state.showDropdown}
+              />
+            ) : null}
+          </div>
+          <Link to="/signup" className="navigation__items navigation__sign-up">
+            <div>Sign up</div>
+          </Link>
+          <Link to="/signin" className="navigation__items navigation__login">
+            <div>Login</div>
+          </Link>
+          <a href="#" className="navigation__items navigation__button">
+            <div>Become a tasker</div>
+          </a>
+        </div>
+        {/* Default Navigation layout */}
         <div
-          className="navigation__items navigation__categories"
+          className="navigation__items navigation__categories default"
           onMouseEnter={this.mouseEnter}
           onMouseLeave={this.mouseLeave}
           style={{
@@ -58,15 +99,25 @@ class Navigation extends Component {
             />
           ) : null}
         </div>
-        <Link to="/signup" className="navigation__items navigation__sign-up">
+        <Link
+          to="/signup"
+          className="navigation__items navigation__sign-up default"
+        >
           <div>Sign up</div>
         </Link>
-        <Link to="/signin" className="navigation__items navigation__login">
+        <Link
+          to="/signin"
+          className="navigation__items navigation__login default"
+        >
           <div>Login</div>
         </Link>
-        <a href="#" className="navigation__button button">
+        <a href="#" className="navigation__button button default">
           <button>Become a tasker</button>
         </a>
+        {/* Mobile button icon */}
+        <div className="navigation__mobileicon" onClick={this.openMobileNav}>
+          <i class="fa fa-bars" />
+        </div>
       </nav>
     );
   }
