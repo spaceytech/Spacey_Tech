58c640b477ae7fac07b4ce7e7f0afa4eac5dce0b
Zakeria Ali <zak.fined@gmail.com>
2019-03-02 15:22:23 +0000

    Setup redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,39 @@ import { BrowserRouter } from "react-router-dom";
 import Routes from "./Routes";
 import "./style/index.scss";
 
+// Redux
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+let middlewares = [thunk];
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+let store = createStore(
+  persistedReducer,
+  compose(
+    applyMiddleware(...middlewares),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+let persistor = persistStore(store);
+
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
   document.getElementById("root")
 );
