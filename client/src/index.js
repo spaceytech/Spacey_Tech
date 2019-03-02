4fc78ec91c5ae1a59ffc2d4d376f4d7320e2e71c
Zakeria Ali <zak.fined@gmail.com>
2019-03-02 16:54:06 +0000

    Fix Redux setup issue

--- client/src/index.js
+++ client/src/index.js
@@ -5,31 +5,32 @@ import Routes from "./Routes";
 import "./style/index.scss";
 
 // Redux
-import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import { PersistGate } from "redux-persist/integration/react";
+import { createStore, applyMiddleware, compose } from "redux";
+import promiseMiddleware from "redux-promise";
+import reduxThunk from "redux-thunk";
 import rootReducer from "./reducers";
-import thunk from "redux-thunk";
 
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+
+const middlewares = [reduxThunk, promiseMiddleware];
 const persistConfig = {
   key: "root",
   storage
 };
 
-let middlewares = [thunk];
-
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-let store = createStore(
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
   persistedReducer,
-  compose(
-    applyMiddleware(...middlewares),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  {},
+  composeEnhancers(applyMiddleware(...middlewares))
 );
 let persistor = persistStore(store);
-
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
