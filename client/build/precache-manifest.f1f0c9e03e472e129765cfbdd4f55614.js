df16e4069bac2acf409c3894f7da9cacf62e3c50
Zakeria Ali <zak.fined@gmail.com>
2019-03-23 21:31:38 +0000

    Add build folder

---
+++ client/build/precache-manifest.f1f0c9e03e472e129765cfbdd4f55614.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "cb406d44e34af0caedee",
+    "url": "/static/js/main.cb406d44.chunk.js"
+  },
+  {
+    "revision": "1c0e00405f28805261d6",
+    "url": "/static/js/2.1c0e0040.chunk.js"
+  },
+  {
+    "revision": "cb406d44e34af0caedee",
+    "url": "/static/css/main.651e06d6.chunk.css"
+  },
+  {
+    "revision": "1c0e00405f28805261d6",
+    "url": "/static/css/2.a2607fd4.chunk.css"
+  },
+  {
+    "revision": "71f484b2a7a0cdc5980684590d7f1a6b",
+    "url": "/index.html"
+  }
+];
