bb5749b387864499e0c6296552ef450f31bc8b48
Zakeria Ali <zak.fined@gmail.com>
2019-03-24 14:26:48 +0000

    Remove build folder

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,16 +1,6 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
-  app.use(
-    proxy("/auth", {
-      target:
-        "https://5c96a5e52938fa14c59f3762--condescending-khorana-538fdd.netlify.com/"
-    })
-  );
-  app.use(
-    proxy("/api", {
-      target:
-        "https://5c96a5e52938fa14c59f3762--condescending-khorana-538fdd.netlify.com/"
-    })
-  );
+  app.use(proxy("/auth", { target: "http://localhost:3003/" }));
+  app.use(proxy("/api", { target: "http://localhost:3003/" }));
 };
